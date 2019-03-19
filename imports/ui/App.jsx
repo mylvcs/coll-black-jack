45ce143b123fec5115bc9fa84a020ffd6409ba26
Zhou Shang <alex.zhou.shang>
2019-03-19 11:05:15 -0700

    try

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React , {Component} from 'react';
 import Hello from './Hello.jsx';
 import Info from './Info.jsx';
+import AccountsUIWrapper from "./AccountsUIWrapper.js";
+import { Meteor } from "meteor/meteor";
+import Game from "./Game.jsx"; 
 
-const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-  </div>
-);
 
-export default App;
+import HomePage from './containers/HomePage/index';
+import NotFound from './containers/NotFound/index';
+
+
+// export default class MainLayout extends React.Component {
+//   render() {
+//     return (
+//       <Router>
+//         <div>
+//           <Header />
+//           <Switch>
+//             <Route exact path="/" component={HomePage} />
+//             <Route component={NotFound} />
+//           </Switch>
+//         </div>
+//       </Router>
+//     );
+//   }
+// }
+
+
+ export default class App extends Component{
+	render(){
+		return(
+			<div>Black Jack HomePage
+				<AccountsUIWrapper/>
+				{
+					Meteor.user() ? 
+					<div>
+						<h1> testing </h1>
+						
+
+					</div>:
+					<Game /> 
+					//<div> Please log in again</div>
+
+				}
+				<div>Black Jack HomePage
+				</div>
+			</div>
+			
+		);
+	}
+
+}
